683df4f678855c6396fc0364b8a6944598b09c54
Ryan Butler <ryancbutler@users.noreply.github.com>
2020-05-27 21:38:21 +0000

    initial commit

---
+++ buildimage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /mnt
+rm -rf vm
+PACKER_LOG=1 packer build -var serial=$(tty) build.json
+cd vm
+qemu-img convert -f qcow2 -O vmdk -o subformat=streamOptimized cent8.qcow2 disk-ide.vmdk
+#Needed for VMware
+printf '\x03' | dd conv=notrunc of=disk-ide.vmdk bs=1 seek=$((0x4))
+
---
+++ runme.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd docker
+docker build -t packerbuilder .
+cd ../packer
+docker run -v $PWD:/mnt --privileged -it packerbuilder /mnt/buildimage.sh
