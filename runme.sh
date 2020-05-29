1d8b4389f7f75b0a3064701cf2a4c2a1be54a93e
Ryan Butler <ryan@ryancbutler.com>
2020-05-29 15:31:36 -0500

    docker cleanup

--- runme.sh
+++ runme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#cd docker
-#docker build -t packerbuilder .
-#cd ../packer
-cd packer
+cd docker
+docker build -t packerbuilder .
+cd ../packer
+#cd packer
 docker run -v $PWD:/mnt --privileged -it packerbuilder /mnt/buildimage.sh
